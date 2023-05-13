48e3987586d04190779ee402a9731e9061eccff5
Won Hee, Lee <113645474+DURAM0830@users.noreply.github.com>
2023-05-13 20:34:54 +0900

    코드 수정

--- My School TV Show Project/AirKoreaAPI.py
+++ My School TV Show Project/AirKoreaAPI.py
@@ -7,7 +7,7 @@ print("경기도 성남시 대기질 정보 불러오기를 시작 합니다. (1
 
 def airKorea():
     url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty" # API URL
-    key = "ROzLzalbkICLJf7ClZsO3Dd2UKL3bceROmPaOfbnqFVcXGqYTp2A6PDofvl3hmqg7p3jIgaa0M+LUkrX5KKcEQ==" # API 접근 보안 키
+    key = "개인키" # API 접근 보안 키
 
     params = {
         "returnType" : "json", # JSON 형식
@@ -27,8 +27,8 @@ def airKorea():
         times = i["dataTime"] # 측정 시각변수 저장
         break
 
-    target_image = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\test.png') # 대기질 베이스 이미지 불러오기
-    fontsFolder = 'C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\경기천년체\\TTF\\경기천년제목_Medium.ttf' #글자로 쓸 폰트 경로
+    target_image = Image.open(.\\images\\baseimages\\test.png') # 대기질 베이스 이미지 불러오기
+    fontsFolder = '.\\images\\baseimages\\경기천년체\\TTF\\경기천년제목_Medium.ttf' #글자로 쓸 폰트 경로
     selectedFont =ImageFont.truetype(os.path.join(fontsFolder,fontsFolder),120) #폰트경로과 사이즈를 설정해줍니다.
     selectedFont1 =ImageFont.truetype(os.path.join(fontsFolder,fontsFolder),90) #폰트경로과 사이즈를 설정해줍니다.
     selectedFont2 =ImageFont.truetype(os.path.join(fontsFolder,fontsFolder),55) #폰트경로과 사이즈를 설정해줍니다.
@@ -37,19 +37,19 @@ def airKorea():
     #========================================================================================== 미세먼지 측정 값에 따른 분류
 
     if (float(pm10) <= 30.0):
-        add_image1 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\good.png')
+        add_image1 = Image.open('.\\images\\baseimages\\good.png')
         pm10result = str(pm10) + "\n\n좋음"
         draw.text((230,375),f"{pm10result}",fill="black",font=selectedFont,align='center') 
     elif (float(pm10) <= 80.0):
-        add_image1 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\middle.png')
+        add_image1 = Image.open('.\\images\\baseimages\\middle.png')
         pm10result = str(pm10) + "\n\n보통"
         draw.text((230,375),f"{pm10result}",fill="black",font=selectedFont,align='center') 
     elif (float(pm10) <= 150.0):
-        add_image1 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\bad.png')
+        add_image1 = Image.open('.\\images\\baseimages\\bad.png')
         pm10result = str(pm10) + "\n\n나쁨"
         draw.text((230,375),f"{pm10result}",fill="black",font=selectedFont,align='center') 
     else:
-        add_image1 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\verybad.png')
+        add_image1 = Image.open('.\\images\\baseimages\\verybad.png')
         pm10result = str(pm10)
         draw.text((230,375),f"{pm10result}",fill="black",font=selectedFont,align='center') 
         draw.text((180,575),f"매우나쁨",fill="black",font=selectedFont1,align='center') 
@@ -57,19 +57,19 @@ def airKorea():
     #========================================================================================== 초미세먼지 측정 값에 따른 분류
 
     if (float(pm25) <= 15.0):
-        add_image2 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\good.png')
+        add_image2 = Image.open('.\\images\\baseimages\\good.png')
         pm25result = str(pm25) + "\n\n좋음"
         draw.text((850,375),f"{pm25result}",fill="black",font=selectedFont,align='center') 
     elif (float(pm25) <= 35.0):
-        add_image2 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\middle.png')
+        add_image2 = Image.open('.\\images\\baseimages\\middle.png')
         pm25result = str(pm25) + "\n\n보통"
         draw.text((850,375),f"{pm25result}",fill="black",font=selectedFont,align='center') 
     elif (float(pm25) <= 75.0):
-        add_image2 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\bad.png')
+        add_image2 = Image.open('.\\images\\baseimages\\bad.png')
         pm25result = str(pm25) + "\n\n나쁨"
         draw.text((850,375),f"{pm25result}",fill="black",font=selectedFont,align='center') 
     else:
-        add_image2 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\verybad.png')
+        add_image2 = Image.open('.\\images\\baseimages\\verybad.png')
         pm25result = str(pm25)
         if len(pm25result) >= 3:
             draw.text((845,375),f"{pm25result}",fill="black",font=selectedFont,align='center') 
@@ -80,19 +80,19 @@ def airKorea():
     #========================================================================================== 오존 측정 값에 따른 분류
 
     if (float(o3) <= 0.030):
-        add_image3 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\ozon.png')
+        add_image3 = Image.open('.\\images\\baseimages\\ozon.png')
         o3result = str(o3) + "\n\n좋음"
         draw.text((1425,375),f"{o3result}",fill="black",font=selectedFont,align='center') 
     elif (float(o3) <= 0.090):
-        add_image3 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\middle.png')
+        add_image3 = Image.open('.\\images\\baseimages\\middle.png')
         o3result = str(o3) + "\n\n보통"
         draw.text((1425,375),f"{o3result}",fill="black",font=selectedFont,align='center') 
     elif (float(o3) <= 0.150):
-        add_image3 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\bad.png')
+        add_image3 = Image.open('.\\images\\baseimages\\bad.png')
         o3result = str(o3) + "\n\n나쁨"
         draw.text((1425,375),f"{o3result}",fill="black",font=selectedFont,align='center') 
     else:
-        add_image3 = Image.open('C:\\Users\\user\\Desktop\\slide show\\images\\baseimages\\verybad.png')
+        add_image3 = Image.open('.\\images\\baseimages\\verybad.png')
         o3result = str(o3)
         draw.text((1425,375),f"{o3result}",fill="black",font=selectedFont,align='center') 
         draw.text((1425,575),f"매우나쁨",fill="black",font=selectedFont1,align='center') 
@@ -105,7 +105,7 @@ def airKorea():
 
     draw.text((50,1020),f"{times}",fill="white",font=selectedFont2,align='left')
 
-    target_image.save("C:\\Users\\user\\Desktop\\slide show\\images\\sky.png")
+    target_image.save(".\\images\\sky.png")
     target_image.close()
 
     print(f"[{datetime.now()}] 미세먼지 정보 업데이트 완료")
